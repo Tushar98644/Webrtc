1c68f212df742e49d3e5763836aca50765311a5c
Tushar Banik <b.toshar@iitg.ac.in>
2024-12-23 23:37:38 +0530

    MediaStream api methods

--- dist/client.js
+++ dist/client.js
@@ -13,16 +13,63 @@ const hasUserMedia = () => {
 };
 const initializeMedia = () => __awaiter(void 0, void 0, void 0, function* () {
     if (!hasUserMedia()) {
-        console.log('Webrtc not supported by browser!');
+        console.log("Webrtc not supported by browser!");
     }
     try {
-        const stream = yield navigator.mediaDevices.getUserMedia({ video: true, audio: true });
-        const video = document.querySelector('video');
+        const stream = yield navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: false,
+        });
+        const video = document.querySelector("video");
         if (video)
             video.srcObject = stream;
         else
-            console.log('video element not found!');
-        console.log('Media stream tracks:', stream.getTracks());
+            console.log("video element not found!");
+        console.log("Media stream tracks:", stream.getTracks());
+        const btnGetAudioTracks = document.getElementById("btnGetAudioTracks");
+        const btnGetTrackById = document.getElementById("btnGetTrackById");
+        const btnGetTracks = document.getElementById("btnGetTracks");
+        const btnGetVideoTracks = document.getElementById("btnGetVideoTracks");
+        const btnRemoveAudioTrack = document.getElementById("btnRemoveAudioTrack");
+        const btnRemoveVideoTrack = document.getElementById("btnRemoveVideoTrack");
+        const btnAddAudioTrack = document.getElementById("btnAddAudioTrack");
+        const btnAddVideoTrack = document.getElementById("btnAddVideoTrack");
+        btnGetAudioTracks.addEventListener("click", () => {
+            console.log("getAudioTracks");
+            console.log(stream.getAudioTracks());
+        });
+        btnGetTrackById.addEventListener("click", () => {
+            console.log("getTrackById");
+            console.log(stream.getTrackById(stream.getAudioTracks()[0].id));
+        });
+        btnGetTracks.addEventListener("click", () => {
+            console.log("getTracks()");
+            console.log(stream.getTracks());
+        });
+        btnGetVideoTracks.addEventListener("click", () => {
+            console.log("getVideoTracks()");
+            console.log(stream.getVideoTracks());
+        });
+        btnRemoveAudioTrack.addEventListener("click", () => {
+            console.log("removeAudioTrack()");
+            stream.removeTrack(stream.getAudioTracks()[0]);
+        });
+        btnRemoveVideoTrack.addEventListener("click", () => {
+            console.log("removeVideoTrack()");
+            stream.removeTrack(stream.getVideoTracks()[0]);
+        });
+        btnAddAudioTrack.addEventListener("click", () => {
+            console.log("addAudioTrack()");
+            navigator.mediaDevices.getUserMedia({ audio: true }).then((audioStream) => {
+                stream.addTrack(audioStream.getAudioTracks()[0]);
+            });
+        });
+        btnAddVideoTrack.addEventListener("click", () => {
+            console.log("addVideoTrack()");
+            navigator.mediaDevices.getUserMedia({ video: true }).then((videoStream) => {
+                stream.addTrack(videoStream.getVideoTracks()[0]);
+            });
+        });
     }
     catch (error) {
         console.log(`Error in getting media stream tracks`, error);
--- src/client.ts
+++ src/client.ts
@@ -1,24 +1,95 @@
-const hasUserMedia = ():Boolean => {
-    return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+const hasUserMedia = (): Boolean => {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 };
 
 const initializeMedia = async () => {
+  if (!hasUserMedia()) {
+    console.log("Webrtc not supported by browser!");
+  }
 
-    if (!hasUserMedia()) {
-        console.log('Webrtc not supported by browser!');
-    }
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: true,
+      audio: false,
+    });
 
-    try {
-       const stream = await navigator.mediaDevices.getUserMedia({video:true,audio:true});
-       const video = document.querySelector('video') as HTMLVideoElement;
-       if (video) video.srcObject = stream;
-       else console.log('video element not found!');
-       console.log('Media stream tracks:', stream.getTracks());
-    }
+    const video = document.querySelector("video") as HTMLVideoElement;
+    if (video) video.srcObject = stream;
+    else console.log("video element not found!");
+    console.log("Media stream tracks:", stream.getTracks());
+
+    const btnGetAudioTracks = document.getElementById(
+      "btnGetAudioTracks"
+    ) as HTMLButtonElement;
+    const btnGetTrackById = document.getElementById(
+      "btnGetTrackById"
+    ) as HTMLButtonElement;
+    const btnGetTracks = document.getElementById(
+      "btnGetTracks"
+    ) as HTMLButtonElement;
+    const btnGetVideoTracks = document.getElementById(
+      "btnGetVideoTracks"
+    ) as HTMLButtonElement;
+    const btnRemoveAudioTrack = document.getElementById(
+      "btnRemoveAudioTrack"
+    ) as HTMLButtonElement;
+    const btnRemoveVideoTrack = document.getElementById(
+      "btnRemoveVideoTrack"
+    ) as HTMLButtonElement;
+    const btnAddAudioTrack = document.getElementById(
+        "btnAddAudioTrack"
+    ) as HTMLButtonElement;
+    const btnAddVideoTrack = document.getElementById(
+        "btnAddVideoTrack"
+    ) as HTMLButtonElement;
+
+    btnGetAudioTracks.addEventListener("click", () => {
+      console.log("getAudioTracks");
+      console.log(stream.getAudioTracks());
+    });
+
+    btnGetTrackById.addEventListener("click", () => {
+      console.log("getTrackById");
+      console.log(stream.getTrackById(stream.getAudioTracks()[0].id));
+    });
+
+    btnGetTracks.addEventListener("click", () => {
+      console.log("getTracks()");
+      console.log(stream.getTracks());
+    });
+
+    btnGetVideoTracks.addEventListener("click", () => {
+      console.log("getVideoTracks()");
+      console.log(stream.getVideoTracks());
+    });
+
+    btnRemoveAudioTrack.addEventListener("click", () => {
+      console.log("removeAudioTrack()");
+      stream.removeTrack(stream.getAudioTracks()[0]);
+    });
+
+    btnRemoveVideoTrack.addEventListener("click", () => {
+      console.log("removeVideoTrack()");
+      stream.removeTrack(stream.getVideoTracks()[0]);
+    });
+
+    btnAddAudioTrack.addEventListener("click", () => {
+      console.log("addAudioTrack()");
+      navigator.mediaDevices.getUserMedia({audio: true}).then((audioStream) => {
+        stream.addTrack(audioStream.getAudioTracks()[0]);
+      });
+    });
+
+    btnAddVideoTrack.addEventListener("click", () => {
+      console.log("addVideoTrack()");
+      navigator.mediaDevices.getUserMedia({video: true}).then((videoStream) => {
+        stream.addTrack(videoStream.getVideoTracks()[0]);
+      });
+    });
     
-    catch (error) {
-        console.log(`Error in getting media stream tracks`, error);
-    }
-}
+  } catch (error) {
+    console.log(`Error in getting media stream tracks`, error);
+  }
+};
 
 initializeMedia();
