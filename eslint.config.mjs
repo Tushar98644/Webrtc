dde43cf275dadf1dbb36fc691b944fe2f787d794
Tushar Banik <b.toshar@iitg.ac.in>
2024-12-27 22:59:18 +0530

    localstream

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,10 @@
-// import typescriptPlugin from "@typescript-eslint/eslint-plugin";
-// import prettierConfig from "eslint-config-prettier";
-// import pkg from 'eslint-plugin-prettier';
-
-// const { eslintPluginPrettier } = pkg;
+import tsParser from '@typescript-eslint/parser'
 
 export default [
   {
     files: ["**/*.{js,jsx,ts,tsx}"], 
     languageOptions: {
+      parser: tsParser,
       parserOptions: {
         ecmaVersion: 2021,
         sourceType: "module",
@@ -21,17 +18,5 @@ export default [
         navigator: true,
       },
     },
-    // plugins: {
-    //   prettier: eslintPluginPrettier,
-    //   "@typescript-eslint": typescriptPlugin,
-    // },
-    // rules: {
-    //   ...typescriptPlugin.configs.recommended.rules, // Recommended TypeScript rules
-    //   ...prettierConfig.rules, // Prettier rules for formatting
-    //   "prettier/prettier": "error", // Ensure Prettier rules are enforced
-    //   "no-console": "warn", // Warn about `console.log`
-    //   "prefer-const": "error", // Use `const` where possible
-    //   "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }], // Ignore `_` in unused vars
-    // },
   },
 ];
